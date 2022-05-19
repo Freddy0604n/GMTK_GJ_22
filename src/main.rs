61b3859648940333b1801c96fac509b9132f298e
Frank Loijenga <fjloijenga@gmail.com>
2022-05-19 18:46:13 +0200

    A pixel moves according to WASD

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate minifb;
 
 use minifb::{Key, Window, WindowOptions, Scale, ScaleMode};
 
-const WIDTH: usize = 1200;
-const HEIGHT: usize = 800;
+const WIDTH: usize = 160;
+const HEIGHT: usize = 80;
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
@@ -12,9 +12,9 @@ fn main() {
         borderless: true,
         title: true,
         resize: true,
-        scale: Scale::X1,
-        scale_mode: ScaleMode::Center,
-        topmost: true,
+        scale: Scale::X8,
+        scale_mode: ScaleMode::Stretch,
+        topmost: false,
         transparency: false,
         none: false
     };
@@ -29,13 +29,32 @@ fn main() {
     });
 
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
-    let mut counter = 0;
+    enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    let mut dir = Direction::Right;
+    let mut position = 1;
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 0xFF0000;
+        if window.is_key_down(Key::W) {
+            dir = Direction::Up;
+        } else if window.is_key_down(Key::S) {
+            dir = Direction::Down;
+        } else if window.is_key_down(Key::D) {
+            dir = Direction::Right;
+        } else if window.is_key_down(Key::A) {
+            dir = Direction::Left;
         }
-
+        buffer[position] = 0;
+        match dir {
+            Direction::Up => position -= WIDTH,
+            Direction::Down => position += WIDTH,
+            Direction::Left => position -= 1,
+            Direction::Right => position += 1,
+        }
+        buffer[position] = 0xFFFFFF;
         window.
             update_with_buffer(&buffer, WIDTH, HEIGHT)
             .unwrap();
