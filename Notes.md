adb7b5db1e8288dd99e3df752dd270c36c128218
Frank Loijenga <fjloijenga@gmail.com>
2022-05-19 08:06:49 +0200

    Added window and changed dependencies

--- Notes.md
+++ Notes.md
@@ -1,6 +1,5 @@
 # NOTES
 
 Dependacies:
-    - blit (2d image renderer)
     - qoi (quite ok image encoder and decoder, is used for its performance)
-    - gilrs (game input, so it can run on multiple platforms)
+    - minifb (window crate with input)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-extern crate blit;
-extern crate gilrs;
-extern crate qoi;
+extern crate minifb;
+
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 800;
 
 fn main() {
-    println!("Hello, GMTK!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "GMTK Test",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut counter = 0;
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0xFFFFFF;
+        }
+
+        window.
+            update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
 }
