be7e63ff47409e6cb9d71417bfa78e55839500f1
Frank Loijenga <fjloijenga@gmail.com>
2022-05-19 22:53:53 +0200

    Tail collision added and apple consumption

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ fn main() {
     });
 
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600 * 2)));
+    #[derive(PartialEq)]
     enum Direction {
         Up,
         Down,
@@ -31,26 +32,30 @@ fn main() {
     }
 
     let mut dir = Direction::Right;
-    let mut prev_dir = Direction::Left;
     let mut head_position = 2;
     let mut tail: Vec<usize> = vec![0, 1];
+    let mut apple = 400;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        if window.is_key_down(Key::W) {
+        if window.is_key_down(Key::W) && dir != Direction::Down {
             // Controls
             dir = Direction::Up;
-        } else if window.is_key_down(Key::S) {
+        } else if window.is_key_down(Key::S) && dir != Direction::Up {
             dir = Direction::Down;
-        } else if window.is_key_down(Key::D) {
+        } else if window.is_key_down(Key::D) && dir != Direction::Left{
             dir = Direction::Right;
-        } else if window.is_key_down(Key::A) {
+        } else if window.is_key_down(Key::A) && dir != Direction::Right{
             dir = Direction::Left;
         }
 
         buffer[head_position] = 0; // clear the previous head_position
         tail.push(head_position);
-        buffer[tail[0]] = 0;
-        tail.drain(..1);
+        if head_position != apple {
+            buffer[tail[0]] = 0;
+            tail.drain(..1);
+        } else {
+            apple = 800;
+        }
 
         match dir {
             Direction::Up => {
@@ -58,7 +63,7 @@ fn main() {
                     // if the head_position needs to roll over
                     head_position = WIDTH * HEIGHT - (WIDTH - head_position);
                 } else {
-                    head_position -= WIDTH;
+                    head_position = head_position - WIDTH;
                 }
             }
             Direction::Down => {
@@ -88,11 +93,21 @@ fn main() {
                 }
             }
         }
+
+        if buffer[head_position] == 0xFFFF00 {  // check if head collided with tail and reset
+            head_position = 2;
+            for i in tail.iter() {
+                buffer[*i] = 0;
+            }
+            tail = vec![0, 1];
+            apple = 400;
+        }
         buffer[head_position] = 0xFFFF00;
 
         for i in tail.iter() {
             buffer[*i] = 0xFFFF00;
         }
+        buffer[apple] = 0xFF0000;
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
