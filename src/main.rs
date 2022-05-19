7393ff31df992a6a19a003e426ca417b59dfb0cb
Frank Loijenga <fjloijenga@gmail.com>
2022-05-19 08:33:05 +0200

    Set windowoptions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 extern crate minifb;
 
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, Window, WindowOptions, Scale, ScaleMode};
 
-const WIDTH: usize = 800;
+const WIDTH: usize = 1200;
 const HEIGHT: usize = 800;
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
+    let options = WindowOptions {
+        borderless: true,
+        title: true,
+        resize: true,
+        scale: Scale::X1,
+        scale_mode: ScaleMode::Center,
+        topmost: true,
+        transparency: false,
+        none: false
+    };
     let mut window = Window::new(
         "GMTK Test",
         WIDTH,
         HEIGHT,
-        WindowOptions::default(),
+        options,
     )
     .unwrap_or_else(|e| {
         panic!("{}", e);
@@ -23,7 +33,7 @@ fn main() {
     let mut counter = 0;
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for i in buffer.iter_mut() {
-            *i = 0xFFFFFF;
+            *i = 0xFF0000;
         }
 
         window.
