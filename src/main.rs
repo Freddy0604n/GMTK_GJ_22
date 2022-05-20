a5ddfc341796beac52260a28d81e7dac1b4a5f4d
Frank Loijenga <fjloijenga@gmail.com>
2022-05-20 07:54:39 +0200

    added camera

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ const HEIGHT: usize = 320;
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut background: Vec<u32> = vec![0; WIDTH * HEIGHT * 2];
+
+    let mut camera = (WIDTH/ 2) * (HEIGHT / 2);
+
     let options = WindowOptions {
         borderless: true,
         title: true,
@@ -25,23 +28,21 @@ fn main() {
 
     window.limit_update_rate(Some(std::time::Duration::from_micros(166000)));
 
-    for i in 0 .. WIDTH * HEIGHT * 2 {
+    for i in 0..WIDTH * HEIGHT * 2 {
         if i % 5 != 0 {
             background[i] = 0xFFFFFF;
         }
-        if i as isize -1 % WIDTH as isize == 0 {
+        if i as isize - 1 % WIDTH as isize == 0 {
             background[i] = 0xFF0000;
         }
     }
+    buffer[camera] = 0xFFFF00;
+    buffer[camera - WIDTH / 2 - HEIGHT / 2] = 0xFFFF00;
+    buffer[camera + WIDTH / 2 - HEIGHT / 2] = 0xFFFF00;
+    buffer[camera - WIDTH / 2 + HEIGHT / 2] = 0xFFFF00;
+    buffer[camera + WIDTH / 2 + HEIGHT / 2] = 0xFFFF00;
     let position = 0;
     while window.is_open() {
-        let mut line = 0;
-        for i in 0 .. WIDTH * HEIGHT {
-            if i % WIDTH == 0 {
-                line += 1;
-            }
-            buffer[i] = background[i];
-        }
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
